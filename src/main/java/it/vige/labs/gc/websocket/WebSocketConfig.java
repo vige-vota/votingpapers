c51cebfb948482ba1c1cc25b6e29c11d9d929268
flashboss <luca.stancapiano@vige.it>
2019-10-27 23:35:14 -0500

    environment profiles

--- src/main/java/it/vige/labs/gc/websocket/WebSocketConfig.java
+++ src/main/java/it/vige/labs/gc/websocket/WebSocketConfig.java
@@ -1,10 +1,13 @@
 package it.vige.labs.gc.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
@@ -13,6 +16,8 @@ import it.vige.labs.gc.JavaAppApplication;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	
+	private final static int BUFFER_SIZE = 32768000;
 
 	/**
 	 * Register Stomp endpoints: the url to open the WebSocket connection.
@@ -24,6 +29,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 		registry.addEndpoint(JavaAppApplication.BROKER_NAME)
 				.setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy)).setAllowedOrigins("*");
+	}
+
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+		registration.setMessageSizeLimit(BUFFER_SIZE); // default : 64 * 1024
+	}
+	
+	@Bean
+	public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+	    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+	    container.setMaxTextMessageBufferSize(BUFFER_SIZE);
+	    return container;
 	}
 
 }
