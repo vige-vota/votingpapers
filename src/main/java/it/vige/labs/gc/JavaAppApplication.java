9e92e4ded54b888c2ed3f92833a96d95c804749f
Luca Stancapiano <lucastancapiano@MacBook-Pro.wind3.hub>
2022-07-02 11:31:58 +0200

    spring boot upgrade

--- src/main/java/it/vige/labs/gc/JavaAppApplication.java
+++ src/main/java/it/vige/labs/gc/JavaAppApplication.java
@@ -3,10 +3,14 @@ package it.vige.labs.gc;
 import static java.lang.Long.MAX_VALUE;
 import static org.springframework.boot.SpringApplication.run;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class JavaAppApplication {
 
@@ -22,5 +26,10 @@ public class JavaAppApplication {
 		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
 		commonsMultipartResolver.setMaxUploadSize(MAX_VALUE);
 		return commonsMultipartResolver;
+	}
+
+	@Bean
+	public KeycloakConfigResolver KeycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
 	}
 }
